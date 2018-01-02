6acbf33288c128ead5b1a3c98131863242babbd4
ani <anna.unton@yahoo.com>
2018-01-02 19:38:04 +0700

    initial comit

---
+++ program.js
@@ -0,0 +1,26 @@
+process.stdin.setEncoding('utf-8');
+
+process.stdin.on('readable', function() {
+	
+	var input = process.stdin.read();
+
+	if (input!== null) {
+		var instruction = input.toString().trim();
+
+		switch(instruction) {
+			case '/exit':
+				process.stdout.write('Quitting app!\n');
+				process.exit();
+				break;
+
+			case 'node version':
+				console.log(process.versions);
+				break;
+
+			default:
+				process.stderr.write('Wrong instruction\n');
+		};
+	}
+});
+
+
